b8e93d833f08014d2da28a52bc887ec1fc14d7f0
anjaneyulu <anjaneyulu.batta@micropyramid.com>
2017-06-09 12:41:44 +0530

    settings updated

--- sandbox/test_django_simple_forum/settings.py
+++ sandbox/test_django_simple_forum/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'test_django_simple_forum.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
